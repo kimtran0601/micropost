853c2fcb20af98b14552e6b77b963bfa1705a0f2
Kim Tran <kdt46@case.edu>
2023-04-26 14:59:32 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = config.port;
 
 console.log(config.mongodbUri);
 mongoose
-	.connect(`${config.mongodbUri}`, { useNewUrlParser: true })
+	.connect(`${config.mongodbUri}`, {useNewUrlParser:true, useUnifiedTopology: true})
 	.then(() => {
 		app.use(bodyParser.json());
         app.use(cors());
